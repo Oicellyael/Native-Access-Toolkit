5fea37b1ce4337d007f2157a194a455f5563e0de
Oicellyael <149580998+Oicellyael@users.noreply.github.com>
2026-03-30 16:01:57 +0200

    Implement additional syscall procedures in indirect.asm
    Added new syscall procedures for reading, writing, and thread creation.

--- indirect.asm
+++ indirect.asm
@@ -1,6 +1,14 @@
-extern pebBase : qword
-extern g_ssn:DWORD
-extern g_syscallAddr:QWORD
+EXTERN pebBase : qword
+
+EXTERN g_ssn:DWORD
+EXTERN g_syscallAddr:QWORD
+
+EXTERN g_ssn_read:DWORD
+EXTERN g_ssn_write:DWORD
+EXTERN g_ssn_thread:DWORD
+EXTERN g_ssn_QSI:DWORD
+EXTERN g_ssn_QIP:DWORD
+
 
 .code
 
@@ -15,5 +23,35 @@ Syscall_NtOpenProcess PROC
 	mov eax, g_ssn     
     jmp qword ptr [g_syscallAddr]
 Syscall_NtOpenProcess ENDP
+
+Syscall_NtReadVirtualMemory PROC
+    mov r10, rcx            
+    mov eax, g_ssn_read     
+    jmp qword ptr [g_syscallAddr] 
+Syscall_NtReadVirtualMemory ENDP
+
+Syscall_NtWriteVirtualMemory PROC
+    mov r10, rcx
+    mov eax, g_ssn_write
+    jmp qword ptr [g_syscallAddr]
+Syscall_NtWriteVirtualMemory ENDP
+
+Syscall_NtCreateThreadEx PROC
+    mov r10, rcx
+    mov eax, g_ssn_thread
+    jmp qword ptr [g_syscallAddr]
+Syscall_NtCreateThreadEx ENDP
+
+Syscall_NtQuerySystemInformation PROC
+    mov r10, rcx
+    mov eax, g_ssn_QSI
+    jmp qword ptr [g_syscallAddr]
+Syscall_NtQuerySystemInformation ENDP
+
+Syscall_NtQueryInformationProcess PROC
+    mov r10, rcx
+    mov eax, g_ssn_QIP
+    jmp qword ptr [g_syscallAddr]
+Syscall_NtQueryInformationProcess ENDP
 
 END
